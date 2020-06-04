67434b6bfd851625fe0db030279cb93071f5563e
Daniel Kuang <danielkuang7@github.com>
2020-06-04 08:39:14 -0400

    Created html and js files

---
+++ index.js
@@ -0,0 +1,13 @@
+var c = document.createElement("canvas");
+var ctx = c.getContext("2d");
+c.width = 500;
+c.height = 350;
+document.body.appendChild(c);
+
+function loop(){
+    ctx.fillStyle = "#19f";
+    ctx.fillRect(0,0,c.width, c.height);
+    requestAnimationFrame(loop);
+}
+
+loop();
