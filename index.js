3ac92da773cf153558fd9b1ed1f77627d5ad207f
Daniel Kuang <danielkuang7@github.com>
2020-07-14 08:34:22 -0400

    Adjusted yspeed

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var player = new function(){
             this.ySpeed -= 0.1;
         }else{
             this.y = p1 - 15;
-            this.ySpeed = 0;
+            this.ySpeed = this.y -(p1-12);
         }
         this.y -= this.ySpeed;
         ctx.save();
