aec559c27af1daec1b39514f2b007be1c3c476f9
Daniel Kuang <danielkuang7@github.com>
2020-07-16 08:33:28 -0400

    Changed p2 t

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var player = new function(){
     this.img.src = "https://i.ebayimg.com/images/g/eVEAAOxypthRt3aV/s-l300.jpg";
     this.draw = function(){
         var p1 = c.height - noise(t + this.x) * 0.25;
-        var p2 = c.height - noise(t + this.x) * 0.25;
+        var p2 = c.height - noise(t+5 + this.x) * 0.25;
         if(p1 - 15> this.y){
             this.ySpeed += 0.1;
         }else{
